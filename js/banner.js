0ceeb4b5d17d209c521a6dbd80ea3768adc86461
raaz2507 <raaz2507@gmila.com>
2025-05-09 21:03:40 +0530

    fix banner path

--- js/banner.js
+++ js/banner.js
@@ -1,15 +1,15 @@
 imgArre = [
-	"./../img/banner/banerImg1.jpg",
-	"./../img/banner/banerImg2.jpg",
-	"./../img/banner/banerImg3.jpg",
-	"./../img/banner/banerImg4.png",
+	"banner/banerImg1.jpg",
+	"banner/banerImg2.jpg",
+	"banner/banerImg3.jpg",
+	"banner/banerImg4.png",
 ];
 const banner = document.getElementById("BanerImg");
 let index = 0;
 function changeBanner(banner) {
 	console.log(banner.src);
 
-	banner.src = imgArre[index];
+	banner.src = `./img/${imgArre[index]}`;
 	banner.alt = imgArre[index];
 	// banner.style.backgroundImage= `url('${imgArre[index]}')`;
 	index = ++index % imgArre.length;
